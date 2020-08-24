f7e005732bb623bfc0ed9e1bc73c00da9dad9184
Mostich <kirill.mostimenko@gmail.com>
2020-08-24 21:47:17 +0300

    chore(clear): clear cmd

--- main.nim
+++ main.nim
@@ -1,18 +1,16 @@
-import os
-
+import os, osproc
+let errC = execCmd("clear")
 var files : seq[string]
 for file in walkFiles("*"):
   var file = file & " - " & $getFileSize(file)
   files.add(file)
   #echo file
-echo ""
 
 var dirs : seq[string]
 for dir in walkDirs("*"):
   var dir = "[" & dir & "]"
   dirs.add(dir)
   #echo dir
-echo ""
 
 var all = dirs & files
 
