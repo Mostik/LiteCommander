9b4db252767338305e634dd5427f298175b10058
Mostich <kirill.mostimenko@gmail.com>
2020-08-24 22:14:06 +0300

    new(proc): getFiles, GetDirs

--- main.nim
+++ main.nim
@@ -1,18 +1,30 @@
-import os, osproc
+import os, osproc, terminal
+
 let errC = execCmd("clear")
-var files : seq[string]
-for file in walkFiles("*"):
-  var file = file & " - " & $getFileSize(file)
-  files.add(file)
-  #echo file
+echo ""
 
-var dirs : seq[string]
-for dir in walkDirs("*"):
-  var dir = "[" & dir & "]"
-  dirs.add(dir)
-  #echo dir
+proc getFiles() : seq[string] =
+  var files : seq[string]
+  for file in walkFiles("*"):
+    var file = file & " - " & $getFileSize(file)
+    files.add(file)
+  result = files
 
-var all = dirs & files
+proc getDirs() : seq[string] =
+  var dirs : seq[string]
+  for dir in walkDirs("*"):
+    var dir = "[" & dir & "]"
+    dirs.add(dir)
+  result = dirs
 
-for oll in all:
-  echo oll
+proc output_all() =
+  var all = getDirs() & getFiles()
+  for oll in all:
+    echo oll
+
+while true:
+  let errC = execCmd("clear")
+  echo ""
+  output_all()
+  setCursorPos(0,0)
+  var input_command = readLine(stdin)
