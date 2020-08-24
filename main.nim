f8e0ad3d104fd58fec50e6807e7df5f6d945efe4
Mostich <kirill.mostimenko@gmail.com>
2020-08-24 21:41:31 +0300

    new(size): file size, [folder]

--- main.nim
+++ main.nim
@@ -1,21 +1,20 @@
 import os
 
-var alls : seq[string]
-for all in walkPattern("*"):
-  alls.add(all)
-echo alls
-echo ""
 var files : seq[string]
 for file in walkFiles("*"):
+  var file = file & " - " & $getFileSize(file)
   files.add(file)
   #echo file
 echo ""
 
 var dirs : seq[string]
 for dir in walkDirs("*"):
+  var dir = "[" & dir & "]"
   dirs.add(dir)
   #echo dir
 echo ""
 
 var all = dirs & files
-echo all
+
+for oll in all:
+  echo oll
