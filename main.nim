e0fbd4c7ba2a76ac1608d37ec2f98ac6838104c4
Mostich <kirill.mostimenko@gmail.com>
2020-08-26 15:14:17 +0300

    new(command): commands in readLine

--- main.nim
+++ main.nim
@@ -1,5 +1,6 @@
 import os, osproc, terminal
-
+import re
+#=======================================
 proc clearCmd(line : bool) =
   if line == true:
     let errC = execCmd("clear")
@@ -22,16 +23,46 @@ proc getDirs() : seq[string] =
   result = dirs
 
 proc output_all() =
-  var all = getDirs() & getFiles()
+  var all = getCurrentDir() & getDirs() & getFiles()
   for oll in all:
     echo oll
+
+proc command(input_command : string) =
+  var tokenTWO = re"""[/]([a-z]+)\s+([a-z]+)"""
+  var tokenONE = re"""[/]([a-z]+)"""
+  if input_command =~ tokenTWO:
+    if matches[0] == "quit":
+      clearCmd(false)
+      quit(0)
+    elif matches[0] == "home":
+      setCurrentDir(getHomeDir())
+    else:
+      try:
+        setCurrentDir(input_command)
+      except:
+        discard
+  elif input_command =~ tokenONE:
+    if matches[0] == "quit":
+      clearCmd(false)
+      quit(0)
+    elif matches[0] == "home":
+      setCurrentDir(getHomeDir())
+    else:
+      try:
+        setCurrentDir(input_command)
+      except:
+        discard
+  else:
+    echo "Error)"
+    sleep(3000)
+
+#=======================================
 
 while true:
   clearCmd(true)
   output_all()
   setCursorPos(0,0)
   var input_command = readLine(stdin)
+  command(input_command)
 
-  if input_command == "quit":
-    clearCmd(false)
-    quit(0)
+  
